cda55a8abf73abd9b04b3a00181342c15361ec46
Tushar Mishra <tusharmishra9695@gmail.com>
2024-02-10 14:15:06 +0530

    #01 UI Done and frontend components created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Select from "react-select";
+import { useForm, Controller } from "react-hook-form";
+import { status } from "./utils";
+import GetAllTask from "./components/GetAllTask";
 function App() {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm();
+  function onSubmit(data) {
+    console.log(data, "dayta");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="add_task">
+      <form onSubmit={handleSubmit(onSubmit)} className="form">
+        <p className="task_p"> Task Manager ! Add Task</p>
+
+        <div className="details">
+          <p>Title</p>
+          <input
+            type="text"
+            {...register("title", {
+              required: true,
+            })}
+            placeholder="Enter Title"
+          />
+          {errors.title && <span>This field is required*</span>}
+        </div>
+        <div className="details">
+          <p>Description</p>
+          <input
+            type="text"
+            defaultValue=""
+            {...register("description", {
+              required: true,
+            })}
+            placeholder="Enter Description"
+          />
+          {errors.description && <span>This field is required*</span>}
+        </div>
+        <div className="details">
+          <p>Completion Status</p>
+          <Controller
+            name="status"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <Select
+                options={status}
+                onChange={(val) => onChange(val.value)}
+              />
+            )}
+            rules={{ required: true }}
+          />
+          {errors.status && <span>This field is required*</span>}
+        </div>
+        <input type="submit" className="input_submit" value="Create Task" />
+      </form>
+      <div className="form">
+        <p className="task_p">Task List</p>
+        <GetAllTask />
+      </div>
     </div>
   );
 }
---
+++ src/components/GetAllTask.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function GetAllTask() {
+  return (
+    <div className="list">
+      <h1>01</h1>
+      <h2>Title</h2>
+      <h4>Desciption</h4>
+      <button>Completed</button>
+      <hr />
+    </div>
+  );
+}
